0ee331a1fc73125bf75f53df2a7d77a2355c1d15
Keimancer <senjoukeima@gmail.com>
2023-03-17 16:35:54 -0500

    Fixed a mistake on seeders.

--- database/seeders/20230208032826-users.js
+++ database/seeders/20230208032826-users.js
@@ -1,35 +1,39 @@
 'use strict'
+const uuid = require('uuid')
 const { Op } = require('sequelize')
-const rolesServices = require('../../services/roles.service')
-const usersServices = require('../../services/users.service')
-
-const rolesService = new rolesServices()
-const usersService = new usersServices()
+const { hashPassword } = require('../../libs/bcrypt')
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()    
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    const usersSeeds = [
+      {
+        id: uuid.v4(),
+        first_name: 'Javier',
+        last_name: 'Calva',
+        email: 'example@academlo.com',
+        username: 'example@academlo.com',
+        password: hashPassword('TEST12345'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: uuid.v4(),
+        first_name: 'José',
+        last_name: 'Castillo',
+        email: 'example2@academlo.com',
+        username: 'example2@academlo.com',
+        password: hashPassword('54321TEST'),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ]
+
     try {
-      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
-      const adminUser2 = await usersService.findUserByEmailOr404('example2@academlo.com')
-      const adminRole = await rolesService.findRoleByName('admin')
-      const profiles = [
-        {
-          user_id: adminUser.id,
-          role_id: adminRole.id,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-        {
-          user_id: adminUser2.id,
-          role_id: adminRole.id,
-          created_at: new Date(),
-          updated_at: new Date(),
-        },
-      ]
-      
-      await queryInterface.bulkInsert('profiles', profiles , {transaction})
+      await queryInterface.bulkInsert('users', usersSeeds, { transaction })
+
       await transaction.commit()
     } catch (error) {
       await transaction.rollback()
@@ -37,22 +41,29 @@ module.exports = {
     }
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     const transaction = await queryInterface.sequelize.transaction()
 
+    const userNames = [
+      'example@academlo.com',
+      'example2@academlo.com',
+    ]
+
     try {
-      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
-      const adminUser2 = await usersService.findUserByEmailOr404('example2@academlo.com')
-      const adminRole = await rolesService.findRoleByName('admin')
-      
-      await queryInterface.bulkDelete('profiles', {
-        user_id: [adminUser.id, adminUser2.id],
-        role_id: adminRole.id
-      }, { transaction })
+      await queryInterface.bulkDelete(
+        'users',
+        {
+          username: {
+            [Op.or]: userNames,
+          },
+        },
+        { transaction }
+      )
+
       await transaction.commit()
     } catch (error) {
       await transaction.rollback()
       throw error
     }
-  }
+  },
 }
