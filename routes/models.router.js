8bb5efdbfc180ab748644165b0254abdf331271a
Pseudo <v.pseudo.developer@gmail.com>
2023-03-17 14:20:05 -0500

    feat: swagger ui installation and .yaml file configuration. docs:  documented login.

--- routes/models.router.js
+++ routes/models.router.js
@@ -3,13 +3,16 @@ const express = require('express')
 
 // const isAuthenticatedByPassportJwt = require('../libs/passport')
 
+const docsRouter = require('./docs.routes')
 const routesAuth = require('./auth.routes')
+
 
 function routerModels(app) {
   const router = express.Router()
 
   app.use('/api/v1', router)
   router.use('/auth', routesAuth)
+  router.use('/docs', docsRouter)
 }
 
 module.exports = routerModels
