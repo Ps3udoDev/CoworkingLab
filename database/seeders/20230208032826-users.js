1720680a8ed0648bd35a0cb76e27b30b68c2d91b
Keimancer <senjoukeima@gmail.com>
2023-03-17 16:16:57 -0500

    Added a new admin user for development.

--- database/seeders/20230208032826-users.js
+++ database/seeders/20230208032826-users.js
@@ -1,58 +1,58 @@
 'use strict'
-const uuid = require('uuid')
 const { Op } = require('sequelize')
-const { hashPassword } = require('../../libs/bcrypt')
+const rolesServices = require('../../services/roles.service')
+const usersServices = require('../../services/users.service')
+
+const rolesService = new rolesServices()
+const usersService = new usersServices()
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()
-
-    const usersSeeds = [
-      {
-        id: uuid.v4(),
-        first_name: 'Javier',
-        last_name: 'Calva',
-        email: 'example@academlo.com',
-        username: 'example@academlo.com',
-        password: hashPassword('TEST12345'),
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]
-
+  async up (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()    
     try {
-      await queryInterface.bulkInsert('users', usersSeeds, { transaction })
-
-      await transaction.commit()
-    } catch (error) {
-      await transaction.rollback()
-      throw error
-    }
-  },
-
-  async down(queryInterface, Sequelize) {
-    const transaction = await queryInterface.sequelize.transaction()
-
-    const userNames = [
-      'example@academlo.com',
-    ]
-
-    try {
-      await queryInterface.bulkDelete(
-        'users',
+      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
+      const adminUser2 = await usersService.findUserByEmailOr404('example2@academlo.com')
+      const adminRole = await rolesService.findRoleByName('admin')
+      const profiles = [
         {
-          username: {
-            [Op.or]: userNames,
-          },
+          user_id: adminUser.id,
+          role_id: adminRole.id,
+          created_at: new Date(),
+          updated_at: new Date(),
         },
-        { transaction }
-      )
-
+        {
+          user_id: adminUser2.id,
+          role_id: adminRole.id,
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ]
+      
+      await queryInterface.bulkInsert('profiles', profiles , {transaction})
       await transaction.commit()
     } catch (error) {
       await transaction.rollback()
       throw error
     }
   },
+
+  async down (queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction()
+
+    try {
+      const adminUser = await usersService.findUserByEmailOr404('example@academlo.com')
+      const adminUser2 = await usersService.findUserByEmailOr404('example2@academlo.com')
+      const adminRole = await rolesService.findRoleByName('admin')
+      
+      await queryInterface.bulkDelete('profiles', {
+        user_id: [adminUser.id, adminUser2.id],
+        role_id: adminRole.id
+      }, { transaction })
+      await transaction.commit()
+    } catch (error) {
+      await transaction.rollback()
+      throw error
+    }
+  }
 }
